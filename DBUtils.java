f41067cd7e5fa83692668882246a90e788009e47
Prabhupad7 <venkataramanaiah7.k@gmail.com>
2023-05-02 09:38:57 +0530

    Practicing the cherry pic command

--- DBUtils.java
+++ DBUtils.java
@@ -1,9 +1,10 @@
 Hare Krishna!
 
-<<<<<<< HEAD
+"Testing the Git Stash command"
+
 "Hello I am in the master branch"....
 "Hello I am in The Development branch"
-
+i
 "Hello guys Git commands are very important"
 
 "Hello I am updating this file"....
